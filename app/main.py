3f198edab213a5b2ce72a884c1404612a7d54a93
naoto714714 <kimura.naoto5@gmail.com>
2024-12-30 20:11:14 +0900

    feat: GitHub Pagesでpyxelを動かす

---
+++ app/main.py
@@ -0,0 +1,27 @@
+import math
+
+import pyxel
+
+
+class App:
+    def __init__(self):
+        pyxel.init(160, 120, title="円運動")
+        self.angle = 0
+        self.center_x = 80
+        self.center_y = 60
+        self.radius = 40
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        self.angle += 0.05
+        if self.angle > 2 * math.pi:
+            self.angle -= 2 * math.pi
+
+    def draw(self):
+        pyxel.cls(0)
+        x = self.center_x + self.radius * math.cos(self.angle)
+        y = self.center_y + self.radius * math.sin(self.angle)
+        pyxel.pset(x, y, 11)
+
+
+App()
